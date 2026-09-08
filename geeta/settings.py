ca9dc7fb22d83f08f203d73f4ec167ad31a927f1
sahi karki <karkisahil2003@gmail.com>
2026-09-08 17:06:28 +0530

    Fix settings debug and allowed hosts

--- geeta/settings.py
+++ geeta/settings.py
@@ -30,10 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_@$r9sdo($ecxr1pr4-)shfb4_alu0v-_i$dp861im%*s0h&2@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['*']
 
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 # Application definition
 
 INSTALLED_APPS = [
